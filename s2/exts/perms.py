f677a2eaf8a96417eb0b629263062569222fc33a
slice <ryaneft@gmail.com>
2018-09-10 20:10:32 -0700

    Add permission linter

---
+++ s2/exts/perms.py
@@ -0,0 +1,68 @@
+import discord
+from discord.ext import commands
+from lifesaver.bot import Cog, Context, command
+
+from s2.uploader import upload
+
+
+def humanize_perm(perm: str) -> str:
+    return perm.replace("_", " ").title().replace("Tts", "TTS")
+
+
+class Perms(Cog):
+    @command()
+    @commands.has_permissions(manage_roles=True)
+    @commands.guild_only()
+    async def role_lint(self, ctx: Context):
+        """Shows roles with unnecessary permissions."""
+        reports = []
+
+        for role in ctx.guild.roles:
+            if role == ctx.guild.default_role:
+                continue
+
+            for (perm, value) in role.permissions:
+                default_perm = getattr(ctx.guild.default_role.permissions, perm)
+                if default_perm and value:
+                    reports.append({
+                        'type': 'warning',
+                        'message': f'"{role.name}" doesn\'t need {humanize_perm(perm)}.'
+                    })
+
+        output = ''
+        for report in reports:
+            output += '[{}] {}\n'.format(report['type'].upper(), report['message'])
+
+        link = await upload(output)
+        await ctx.send(f'Linted. {len(reports)} report(s). {link}')
+
+    @command()
+    @commands.guild_only()
+    async def perms(self, ctx: Context, target: discord.Member = None):
+        """Shows permissions for a user."""
+        target = target or ctx.author
+        perms = target.guild_permissions
+        embed = discord.Embed()
+        allowed = []
+        denied = []
+
+        for perm in dir(perms):
+            if "__" in perm:
+                continue
+            value = getattr(perms, perm, None)
+            if not isinstance(value, bool):
+                continue
+            title = humanize_perm(perm)
+            if value:
+                allowed.append(title)
+            else:
+                denied.append(title)
+
+        embed.add_field(name="Allowed", value="\n".join(allowed) or "<none>")
+        embed.add_field(name="Denied", value="\n".join(denied) or "<none>")
+
+        await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Perms(bot))
---
+++ s2/uploader.py
@@ -0,0 +1,14 @@
+__all__ = ['upload']
+
+import aiohttp
+
+ENDPOINT = 'http://mystb.in'
+HEADERS = {'User-Agent': 's2/0.0.0'}
+
+
+async def upload(text: str):
+    async with aiohttp.ClientSession(headers=HEADERS) as sess:
+        resp = await sess.post(f'{ENDPOINT}/documents', data=text)
+        packet = await resp.json()
+        key = packet['key']
+        return f'{ENDPOINT}/{key}.txt'
