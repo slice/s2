b5bbc8a02cf40b01bf310474c6612f4ea2200158
slice <ryaneft@gmail.com>
2018-11-15 22:58:56 -0800

    add bent ext

---
+++ s2/exts/bent.py
@@ -0,0 +1,30 @@
+import typing
+
+import discord
+from lifesaver.bot import Cog
+
+
+def ban_channel_for(guild: discord.Guild) -> typing.Optional[discord.TextChannel]:
+    """Return the ban echoing channel for a :class:`discord.Guild`."""
+    return discord.utils.find(
+        lambda channel: channel.topic is not None and '[s2:bent]' in channel.topic,
+        guild.text_channels,
+    )
+
+
+class Bent(Cog):
+    async def on_member_ban(self, guild, user):
+        channel = ban_channel_for(guild)
+        if not channel:
+            return
+        await channel.send(f'**{user} got bent**')
+
+    async def on_member_unban(self, guild, user):
+        channel = ban_channel_for(guild)
+        if not channel:
+            return
+        await channel.send(f'**{user} got unbent**')
+
+
+def setup(bot):
+    bot.add_cog(Bent(bot))
