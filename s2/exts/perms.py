be2cd43ba6106ebaf797675ad0ada0ddead6e25e
slice <ryaneft@gmail.com>
2019-03-02 14:25:18 -0800

    add role cleaner and revamp role linter

--- s2/exts/perms.py
+++ s2/exts/perms.py
@@ -1,12 +1,35 @@
+import collections
+
 import discord
 from discord.ext import commands
+from lifesaver.utils import pluralize
 from lifesaver.bot import Cog, Context, command
+from jishaku.paginators import PaginatorInterface
 
 from s2.uploader import upload
 
 
 def humanize_perm(perm: str) -> str:
     return perm.replace("_", " ").title().replace("Tts", "TTS")
+
+
+def diff_markers(marker, iterable):
+    return [f'{marker} {value}' for value in iterable]
+
+
+def names_of_perms(perms: discord.Permissions, *, is_granted: bool = True):
+    return {humanize_perm(name) for (name, value) in perms if value is is_granted}
+
+
+def diff_perms(before: discord.Permissions, after: discord.Permissions) -> str:
+    before_n = names_of_perms(before)
+    after_n = names_of_perms(after)
+
+    changes = diff_markers('+', after_n - before_n) \
+        + diff_markers('-', before_n - after_n) \
+        + diff_markers(' ', before_n & after_n)
+
+    return changes
 
 
 class Perms(Cog):
@@ -23,32 +46,96 @@ class Perms(Cog):
                 ctx += f'\N{LOCK WITH INK PEN} unable to flatten {role.name}: {err}'
         await ctx.send_pages()
 
+    @command()
+    @commands.bot_has_permissions(manage_roles=True)
+    @commands.has_permissions(manage_roles=True)
+    @commands.guild_only()
+    async def clean_roles(self, ctx: Context):
+        """Cleans all unnecessary permissions from roles."""
+        everyone = ctx.guild.default_role
+
+        pages = commands.Paginator(prefix='```diff', max_size=1000)
+        interface = PaginatorInterface(ctx.bot, pages, owner=ctx.author)
+
+        pages.add_line('Removing unnecessary permission bits from all roles.', empty=True)
+        await interface.send_to(ctx)
+
+        def needs_cleaning(role):
+            if role == everyone:
+                return False
+
+            has_duplicates = role.permissions.value & everyone.permissions.value != 0
+            can_edit = role.position < ctx.guild.me.top_role.position
+
+            return has_duplicates and can_edit
+
+        cleanable_roles = [
+            role for role in ctx.guild.roles if needs_cleaning(role)
+        ]
+
+        if not cleanable_roles:
+            await interface.add_line('No roles need tidying up!', empty=True)
+
+        for role in cleanable_roles:
+            perms = role.permissions
+
+            # remove all permissions that are already inherited from the default
+            # role
+            cleaned = discord.Permissions(perms.value)
+            cleaned.update(**{
+                name: False
+                for (name, value) in everyone.permissions if getattr(perms, name) == value
+            })
+
+            await interface.add_line(f'@@ {role.name}')
+            await interface.add_line(f'--- {hex(perms.value)} \N{RIGHTWARDS ARROW} {hex(cleaned.value)}')
+            for change in diff_perms(perms, cleaned):
+                await interface.add_line(change)
+            await interface.add_line()
+
+            try:
+                await role.edit(permissions=cleaned)
+                await interface.add_line('+ Successfully cleaned up role.', empty=True)
+            except discord.HTTPException:
+                await interface.add_line('- Failed to clean up role. (Likely a hierarchy problem.)', empty=True)
+
+        await interface.add_line('Done!')
+
     @command(typing=True)
     @commands.has_permissions(manage_roles=True)
     @commands.guild_only()
     @commands.cooldown(1, 5, type=commands.BucketType.guild)
-    async def role_lint(self, ctx: Context):
+    async def lint_roles(self, ctx: Context):
         """Shows roles with unnecessary permissions."""
-        reports = []
+        indent = ' ' * 2
+        lint_results = collections.defaultdict(list)
 
         for role in ctx.guild.roles:
             if role == ctx.guild.default_role:
                 continue
 
+            if not role.members:
+                lint_results[role.name].append('Nobody has this role.')
+
             for (perm, value) in role.permissions:
                 default_perm = getattr(ctx.guild.default_role.permissions, perm)
                 if default_perm and value:
-                    reports.append({
-                        'type': 'warning',
-                        'message': f'"{role.name}" doesn\'t need {humanize_perm(perm)}.'
-                    })
+                    description = f"{humanize_perm(perm)} is unnecessary."
+                    lint_results[role.name].append(description)
+
+        if not lint_results:
+            await ctx.send('this server is flawless!')
+            return
 
         output = ''
-        for report in reports:
-            output += '[{}] {}\n'.format(report['type'].upper(), report['message'])
+        for role_name, descriptions in lint_results.items():
+            output += f'{role_name}\n'
+            output += '\n'.join(f'{indent} - {description}' for description in descriptions)
+            output += '\n\n'
 
         link = await upload(output)
-        await ctx.send(f'linted: {len(reports)} report(s). {link}')
+        roles = pluralize(role=len(lint_results), with_quantity=True)
+        await ctx.send(f'{roles} are :( - {link}')
 
     @command()
     @commands.guild_only()
