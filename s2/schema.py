4e25833b4e4b131c1b59712cac3f5dd029b7a7a1
slice <ryaneft@gmail.com>
2019-08-23 18:53:26 -0700

    schema: make columns NOT NULL

--- s2/schema.py
+++ s2/schema.py
@@ -4,19 +4,19 @@ STATEMENTS = [
     """
     CREATE TABLE IF NOT EXISTS voyager_gets (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        user_id BIGINT,
-        get_message_id BIGINT,
-        voyager_message_id BIGINT,
-        channel_id BIGINT,
-        guild_id BIGINT
+        user_id BIGINT NOT NULL,
+        get_message_id BIGINT NOT NULL,
+        voyager_message_id BIGINT NOT NULL,
+        channel_id BIGINT NOT NULL,
+        guild_id BIGINT NOT NULL
     )
     """,
     """
     CREATE TABLE IF NOT EXISTS voyager_stats (
         user_id BIGINT PRIMARY KEY,
-        total_gets INTEGER,
-        rank INTEGER,
-        last_get TIMESTAMP
+        total_gets INTEGER NOT NULL,
+        rank INTEGER NOT NULL,
+        last_get TIMESTAMP NOT NULL
     )
     """,
 ]
