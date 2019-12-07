7ac5c8d9e4d73b74d04433b97b8bcbd9b7b77838
slice <ryaneft@gmail.com>
2019-12-06 22:22:03 -0800

    hide obvious commands

--- s2/bot.py
+++ s2/bot.py
@@ -4,17 +4,16 @@ import sqlite3
 
 import aiosqlite
 import lifesaver
-from discord.ext import commands
 
 from .schema import STATEMENTS
 
 
 class S2(lifesaver.Bot):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.db = None
 
-    async def setup_db(self):
+    async def setup_db(self) -> None:
         self.db = aiosqlite.connect(
             "s2.db", loop=self.loop, detect_types=sqlite3.PARSE_DECLTYPES
         )
@@ -26,21 +25,32 @@ class S2(lifesaver.Bot):
         # create tables as necessary
         await self.create_tables()
 
-    async def on_ready(self):
+    async def on_ready(self) -> None:
         await super().on_ready()
+
+        self._hide_obvious_commands()
 
         if self.db is None:
             await self.setup_db()
 
-    async def create_tables(self):
+    async def create_tables(self) -> None:
         for statement in STATEMENTS:
             await self.db.execute(statement)
 
         await self.db.commit()
 
-    async def close(self):
+    async def close(self) -> None:
         # disconnect manually
         await self.db.close()
         self.db._connection = None
 
         await super().close()
+
+    def _hide_obvious_commands(self) -> None:
+        for name in {"help", "ping", "rtt", "jishaku"}:
+            self._hide(name)
+
+    def _hide(self, command_name: str) -> None:
+        command = self.get_command(command_name)
+        if command:
+            command.hidden = True
