a763bfe5bf5220bd49fb11657da7360c760bc1cd
slice <ryaneft@gmail.com>
2019-12-06 21:14:10 -0800

    buttons: update 3.8 to 3.9
    well, ostensibly actually. the schedule hasn't been confirmed yet

--- s2/exts/buttons.py
+++ s2/exts/buttons.py
@@ -3,14 +3,14 @@ import datetime
 import lifesaver
 from lifesaver.utils.formatting import human_delta
 
-PY38_RELEASE = datetime.datetime(2019, 10, 21, tzinfo=datetime.timezone.utc)
+NEXT_RELEASE = datetime.datetime(2020, 10, 5, tzinfo=datetime.timezone.utc)
 
 
 class Buttons(lifesaver.Cog):
-    @lifesaver.command(name="3.8", aliases=["38"])
-    async def python_38_when(self, ctx):
-        """python 3.8 when"""
-        delta = PY38_RELEASE - datetime.datetime.now(datetime.timezone.utc)
+    @lifesaver.command(name="3.9", aliases=["39"])
+    async def python_39_when(self, ctx):
+        """python 3.9 when"""
+        delta = NEXT_RELEASE - datetime.datetime.now(datetime.timezone.utc)
         await ctx.send(human_delta(delta))
 
 
