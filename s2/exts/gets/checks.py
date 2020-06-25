d57eac7d73f40c842aa1161a99610ba6aa0243c1
NotNite <hi@notnite.com>
2020-06-25 14:59:33 -0400

    Abolish gets

--- s2/exts/gets/checks.py
+++
@@ -1,8 +0,0 @@
-__all__ = ["get_channel"]
-
-import lifesaver
-
-
-def get_channel(ctx: lifesaver.Context) -> bool:
-    """A check that enforces commands to only be runnable from GET channels."""
-    return ctx.channel.id in ctx.cog.config.channels
