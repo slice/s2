85991d7a7357efd1bb9b019e87cfa5a42f5f95d1
slice <tinyslices@gmail.com>
2021-09-03 13:14:19 -0700

    Fix the schema being plain wrong
    Match the schema closer to the one used in production.

--- db/schema.sql
+++ db/schema.sql
@@ -11,16 +11,5 @@ CREATE TABLE IF NOT EXISTS voyager_stats (
   user_id BIGINT PRIMARY KEY,
   total_gets INTEGER NOT NULL,
   rank INTEGER NOT NULL,
-  last_get TIMESTAMP NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS voyager_social_blocks (
-  blocker_id BIGINT NOT NULL,
-  blockee_id BIGINT NOT NULL,
-  PRIMARY KEY (blocker_id, blockee_id)
-);
-
-CREATE TABLE IF NOT EXISTS voyager_preferences (
-  user_id BIGINT PRIMARY KEY,
-  preferences jsonb NOT NULL DEFAULT '{}'::jsonb 
+  last_get TIMESTAMP
 );
